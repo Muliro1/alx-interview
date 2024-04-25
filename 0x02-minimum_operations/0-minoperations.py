ed8cd17097cbc096b721d7622f9859bdd1428ec4
Muliro1 <mulirokhaemba@gmail.com>
2024-04-25 14:21:07 +0300

    t

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,7 +4,8 @@
 
 
 def minOperations(n: int) -> int:
-    """ Minimum Operations needed to get n H characters """
+    """ Minimum Operations needed 
+    to get n H characters """
     next = 'H'
     body = 'H'
     op = 0
