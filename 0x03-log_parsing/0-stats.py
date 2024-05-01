3df387a264734a7ef2305e8594d6bd2e29e18547
Muliro1 <mulirokhaemba@gmail.com>
2024-05-02 01:49:32 +0300

    parse

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+log parsing
+"""
+
+import sys
+import re
+
+
+def output(log: dict) -> None:
+    """
+    helper function to display stats
+    """
+    print("File size: {}".format(log["file_size"]))
+    for code in sorted(log["code_frequency"]):
+        if log["code_frequency"][code]:
+            print("{}: {}".format(code, log["code_frequency"][code]))
+
+
+if __name__ == "__main__":
+    regex = re.compile(
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
+
+    l_count = 0
+    log = {}
+    log["file_size"] = 0
+    log["code_frequency"] = {
+        str(code): 0 for code in [
+            200, 301, 400, 401, 403, 404, 405, 500]}
+
+    try:
+        for l in sys.stdin:
+            l = l.strip()
+            match = regex.fullmatch(l)
+            if (match):
+                l_count += 1
+                code = match.group(1)
+                file_size = int(match.group(2))
+
+                # File size
+                log["file_size"] += file_size
+
+                # status code
+                if (code.isdecimal()):
+                    log["code_frequency"][code] += 1
+
+                if (l_count % 10 == 0):
+                    output(log)
+    finally:
+        output(log)
