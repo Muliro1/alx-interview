7751cc8d2eda42410f1dda57376a1dea7ac8ecae
Muliro1 <mulirokhaemba@gmail.com>
2024-05-06 04:33:42 +0300

    bits

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -10,11 +10,11 @@ def minOperations(n):
     Returning the fewest number of operations needed to result in exactly
     n H characters in the file.
     """
-    n_operations = 0
+    operations = 0
     min_operations = 2
     while n > 1:
         while n % min_operations == 0:
-            n_operations += min_operations
+            operations += min_operations
             n /= min_operations
         min_operations += 1
-    return n_operations
+    return operations
